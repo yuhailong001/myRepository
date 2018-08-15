439933fca80a40dbec680e31fe883e24dfe19833
yuhailong <1024403858@qq.com>
2018-08-15 10:15:53 +0800

    1

---
+++ java-test01/src/main/java/com/bjpowernode/arraytest/Test1.java
@@ -0,0 +1,16 @@
+package com.bjpowernode.arraytest;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int data[] = {53,436,32,84,9,17,346};
+        sortArray(data);
+    }
+
+    public static void sortArray(int[] data){
+
+
+
+    }
+
+}
