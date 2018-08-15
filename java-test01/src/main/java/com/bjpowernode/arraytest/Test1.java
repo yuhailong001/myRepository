51be3d8a5b6d38b017b70608d12194c33eec1d18
yuhailong <1024403858@qq.com>
2018-08-15 10:33:55 +0800

    2

--- java-test01/src/main/java/com/bjpowernode/arraytest/Test1.java
+++ java-test01/src/main/java/com/bjpowernode/arraytest/Test1.java
@@ -1,5 +1,7 @@
 package com.bjpowernode.arraytest;
 
+import java.util.Arrays;
+
 public class Test1 {
 
     public static void main(String[] args) {
@@ -9,7 +11,18 @@ public class Test1 {
 
     public static void sortArray(int[] data){
 
+        for(int i = 0; i < data.length - 1; i++){
 
+            for(int j = 0; j < data.length - 1; j++){
+                if(data[j+1] - data[j] < 0){
+                    int n = data[j+1];
+                    data[j+1] = data[j];
+                    data[j] = n;
+                }
+
+            }
+            System.out.println(Arrays.toString(data));
+        }
 
     }
 
